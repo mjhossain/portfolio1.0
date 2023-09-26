b959e796eefd7911b55a434761d4b428cb2e342f
mjhossain <mjhossainnyc@gmail.com>
2023-09-26 14:20:13 -0400

    minor chnages

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,11 +2,11 @@
 
 export default function ProjectCard(props) {
     return(
-        <div className="project-card">
+        <div className="project-card"  id={props.position === "left" ? "left" : "right"}>
             <h3>{props.name}</h3>
             <p>{props.desc}</p>
             <div className="tech-stack">
-                
+
             </div>     
         </div>
     )
