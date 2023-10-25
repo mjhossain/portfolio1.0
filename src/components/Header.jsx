0d3c1dbc763f9a6ccf9398bf6507523193af7e28
mjhossain <mjhossainnyc@gmail.com>
2023-10-25 19:42:18 -0400

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,17 @@ import '../css/Header.css'
 export default function Header() {
     return(
         <div className="header">
-            <p><span className="mo">Mo</span>InTech</p>
-            <ul className="navbar">
-                <li><a href="#skills">Skill</a></li>
-                <li><a href="#projects">Projects</a></li>
-            </ul>
+            <div className="title">
+                <p><span className="mo">Mo</span>InTech</p>
+            </div>
+            <div className="navbar-box">
+                <ul className="navbar">
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                </ul>
+            </div>
+            
+            
         </div>
     )
 }
