bdc8f877fe4b03445afc897536cccb62d3f5315d
mjhossain <mjhossainnyc@gmail.com>
2024-01-28 00:11:11 -0500

    certs added

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -5,11 +5,11 @@ import { SiFastapi, SiGnubash } from 'react-icons/si'
 import '../css/logos.css'
 
 export default function Skills() {
-    return(
-        <div className="skills"  id="skills">
+    return (
+        <div className="skills" id="skills">
 
             <div className="skills-content">
-                <h1>Skills // Tools</h1>
+                <h1>Skills // Tools // Certs</h1>
                 <div className="skills-showcase">
                     <div className="box">
                         <h2>Development</h2>
@@ -23,11 +23,29 @@ export default function Skills() {
                         <h2>Security</h2>
                         <p>SIEM, LOG Analysis, Packet Analysis, Security Controls, Security Frameworks</p>
                     </div>
+                    <div className="box-cert">
+                        {/* <h2>Certifications</h2> */}
+                        <div className="certs">
+                            <div className='cert'>
+                                <img src="/images/google-cc-c.png" alt="" />
+                                <a href="https://coursera.org/share/822a8a1177b065ecb0ebca5fb89314e1" target='_blank'>
+                                    Google Cybersecurity Professional
+                                </a>
+                            </div>
+                            <div className='cert'>
+                                <img src="/images/cc-c.png" alt="" />
+                                <a href="https://www.credly.com/badges/a8a930c1-c9df-4852-a6a5-15a45d4743d5/public_url" target='_blank'>
+                                    ISC2 Certified in Cybersecurity
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
 
             <div className="logos-body">
-                <BiLogoReact fill='black' size='8em'className='logos logo-1' /> 
+                <BiLogoReact fill='black' size='8em' className='logos logo-1' />
                 <BiLogoPython fill='black' size='10em' className='logos logo-2' />
                 <BiLogoDocker fill='black' size='10em' className='logos logo-3' />
                 <BiLogoFirebase fill='black' size='10em' className='logos logo-4' />
