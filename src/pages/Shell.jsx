5efaedb23873069e18dc026187b1b7d7a6d538e1
Mohammed <mjhossainnyc@gmail.com>
2024-11-29 21:47:39 -0500

    shell design fixed, next steps is to fix the commands

--- src/pages/Shell.jsx
+++ src/pages/Shell.jsx
@@ -7,22 +7,45 @@ const Shell = () => {
     // Define commands for the terminal
     const commands = {
         whoami: "Mohammed J Hossain",
-        help: "Available commands: help, echo <text>, clear",
-        echo: (args) => args.join(' '),
+        help: "Available commands: help, whoismo, learn <interest/technology>, exit, man <command>",
+        whoismo: "Mohammed",
+        learn: (arg) => {
+            if (arg == 'devops') {
+                return "DevOps"
+            }
+        },
         clear: () => "Type 'clear' in the terminal to reset it.",
+        exit: () => {
+            // Display a message
+            console.log("Exiting... Redirecting to the homepage in 2 seconds.");
+            // Wait 2 seconds before redirecting
+            setTimeout(() => {
+                window.location.href = "/"; // Replace '/' with your homepage route
+            }, 2000); // 2000 milliseconds = 2 seconds
+            return "Exiting in 2 seconds...Good bye! Hope you learned something new about me!";
+        }
     };
 
     return (
-        <div id='terminal'>
-            <h1 style={{ color: "white" }}>Mo's Brain</h1>
-            <ReactTerminal
-                commands={commands}
-                welcomeMessage={`Welcome to the React Terminal!\n\nType 'help' for a list of available commands.\n\nEnjoy exploring the terminal!`}
-                prompt="user@mo-brain:~$"
-                theme="material-dark"
-                showControlBar={false}
-                showControlButtons={false}
-            />
+        <div className='terminal_box'>
+            <div className='terminal'>
+                {/* <h1 style={{ color: "white" }}>Mo's Brain</h1> */}
+                <ReactTerminal
+                    commands={commands}
+                    welcomeMessage={[
+                        "Welcome to Mo's Shell\n",
+                        <br />, <br />,
+                        "Enter 'help' to learn about the available commands",
+                        <br />, <br />,
+                        "",
+                        ""
+                    ]}
+                    prompt="visitor@mo-brain:~$"
+                    theme="material-dark"
+                    showControlBar={true}
+                    showControlButtons={true}
+                />
+            </div>
         </div>
     );
 };
